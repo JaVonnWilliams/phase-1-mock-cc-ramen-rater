940c2da4ac7b4c6fa590b085e8b77a67552a7306
JaVonn Williams <javonn_w@yahoo.com>
2022-12-21 13:17:34 -0800

    final commit

---
+++ notes.md
@@ -0,0 +1,9 @@
+"ramens; [ this is an array of objects]
+{
+      "id": 1,
+      "name": "Shoyu Ramen",
+      "restaurant": "Nonono",
+      "image": "./assets/ramen/shoyu.jpg",
+      "rating": 7,
+      "comment": "Delish. Can't go wrong with a classic!"
+    },
--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-// write your code here
+//globals
+const URL = "http://localhost:3000/ramens";
+
+//DOM selectors
+
+const ramenMenu = document.getElementById("ramen-menu")
+const ramenDetails = document.querySelector("#ramen-details")
+const ramenDetailImage = document.querySelector("#detail-image")
+const comment = document.querySelector("#comment-display")
+const ratingDisplay = document.querySelector("#rating-display")
+const newRamenForm = document.getElementById("new-ramen")
+const RestaurantName = document.querySelector("#restaurant")
+
+
+//Event Listeners?
+
+
+
+
+fetch("http://localhost:3000/ramens")
+.then(response => response.json())
+.then(data => renderRamenIMG(data));
+renderRamenIMG();
+
+//Event Handlers
+
+
+//Render ramen img function
+function renderRamenIMG(ramenObject) {
+remenObject = data.ramenDetailImage
+
+console.log(renderRamenIMG)
+
+
+}
+
+
+
+
+
+
+
+
+
